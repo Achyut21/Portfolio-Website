796b12e0486123e9969871683c8ee6bdf7f04902
Achyut21 <achyutkatiyar21@gmail.com>
2024-07-04 22:19:28 +0530

    Updates 3.1

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,7 @@ import project1 from "../assets/projects/project-1.jpeg"
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+//All the details about the website is mentioned below
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
